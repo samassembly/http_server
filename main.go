91e83520d85c54a1836e7859eae583e14a8c6a7a
SamPCS <svogel@pcsknox.com>
2025-03-26 10:57:24 -0400

    Added handler function for homepage

--- main.go
+++ main.go
@@ -10,7 +10,11 @@ func main() {
 	// Create new ServeMux
 	mux := http.NewServeMux()
 
+	// Create new Fileserver
+	fileServer := http.FileServer(http.Dir("."))
+
 	//Handler functions here (blank for first step)
+	mux.Handle("/", fileServer)
 
 	// Create server struct
 	server := &http.Server{
