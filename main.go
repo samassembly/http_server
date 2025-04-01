0a4beb03a6b4fcbdbb3199f40c385a43a213e99d
SamPCS <svogel@pcsknox.com>
2025-04-01 13:49:34 -0400

    Separated app and api endpoints

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
-
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
+	// Create new apiConfig
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	// Create new ServeMux
 	mux := http.NewServeMux()
 
 	//handler functions 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	// Create server struct
 	server := &http.Server{
@@ -30,6 +41,20 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request ) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r) })
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
 }
