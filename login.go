d984b50eb6d8f03b34f6a0189d1b367764cf8ce4
SamPCS <svogel@pcsknox.com>
2025-04-15 13:31:33 -0400

    Added password verification to login endpoint

--- login.go
+++ login.go
@@ -5,10 +5,10 @@ import (
 	"log"
 	"encoding/json"
 	"github.com/samassembly/http_server/internal/auth"
-	"github.com/samassembly/http_server/internal/database"
+	//"github.com/samassembly/http_server/internal/database"
 )
 
-func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 	//decode request body
 	type parameters struct {
         Email string `json:"email"`
@@ -25,16 +25,39 @@ func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
     }
 
 	//check for user matching email
-	dbUser, err := cfg.databaseQueries.LoginUser(r.Context(), params.email)
+	dbUser, err := cfg.databaseQueries.LoginUser(r.Context(), params.Email)
 	if err != nil {
 		log.Printf("Incorrect Email or Password")
 		w.WriteHeader(401)
-		w.Write("Incorrect Email or Password")
+		w.Write([]byte("Incorrect Email or Password"))
 		return
 	}
 
-	user := User{
-		ID: dbUser.ID,
-		CreatedAt: dbUser
+	//compare input password to stored hash
+	ok := auth.CheckPasswordHash(dbUser.HashedPassword, params.Password)
+	if ok != nil {
+		log.Printf("Passwords do not match: %s", err)
+		w.WriteHeader(401)
+		return
 	}
+
+	//craft response
+	respBody := User{
+		ID: dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email: dbUser.Email,
+	}
+
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+	return
 }
